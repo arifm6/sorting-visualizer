d5e5aacc703b4453d68b66b300472923b0bf535f
Ahmad Arif <arifmassih6@gmail.com>
2023-03-20 23:18:56 -0400

    added quick sort and visualization. not the prettiest but showest low and high index and quickly swaps through

--- src/scripts/algorithms.ts
+++ src/scripts/algorithms.ts
@@ -1,3 +1,5 @@
+import { Frame } from "@/globalState/context";
+
 function getFrame(elements: number[], highlighted: number[]) {
   return { elements, highlighted };
 }
@@ -9,6 +11,12 @@ export function sort(arr: number[], algorithm: string) {
       break;
     case "selection sort":
       frames = [...frames, ...selectionSort(arr)];
+      break;
+    case "quick sort":
+      const tempFrames: Frame[] = [];
+
+      frames = [...frames, ...quickSort(arr, 0, arr.length - 1, frames)];
+      break;
   }
   frames.push(getFrame([], [-1, -1]));
   return frames;
@@ -53,5 +61,77 @@ function selectionSort(arr: number[]) {
     frames.push(getFrame([i, minIndex], [j, minIndex]));
   }
 
+  return frames;
+}
+
+//GFG
+// JavaScript program for implementation of QuickSort
+
+/* This function takes last element
+    as pivot, places the pivot element
+    at its correct position in sorted
+    array, and places all smaller 
+    (smaller than pivot) to left of
+    pivot and all greater elements to 
+    right of pivot */
+
+function partition(arr: number[], low: number, high: number, frames: Frame[]) {
+  let temp;
+  let pivot = arr[high];
+
+  // index of smaller element
+  let i = low - 1;
+  for (let j = low; j <= high - 1; j++) {
+    // If current element is
+    // smaller than or
+    // equal to pivot
+    frames.push(getFrame([], [low, j, high]));
+    if (arr[j] <= pivot) {
+      i++;
+
+      // swap arr[i] and arr[j]
+      frames.push(getFrame([i, j], [low, i, j, high]));
+      temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+  }
+
+  // swap arr[i+1] and arr[high]
+  // (or pivot)
+
+  temp = arr[i + 1];
+  arr[i + 1] = arr[high];
+  arr[high] = temp;
+  frames.push(getFrame([i + 1, high], [low, i + 1, high]));
+
+  return i + 1;
+}
+
+function quickSort(
+  arr: number[],
+  low: number,
+  high: number,
+  frames: Frame[]
+): any {
+  /* The main function that implements
+    QuickSort() arr[] --> Array to be 
+    sorted,
+    low --> Starting index,
+    high --> Ending index */
+
+  if (low < high) {
+    /* pi is partitioning index, 
+            arr[pi] is now at right place */
+    frames.push(getFrame([], [low, high]));
+
+    let pi = partition(arr, low, high, frames);
+
+    // Recursively sort elements
+    // before partition and after
+    // partition
+    quickSort(arr, low, pi - 1, frames);
+    quickSort(arr, pi + 1, high, frames);
+  }
   return frames;
 }
